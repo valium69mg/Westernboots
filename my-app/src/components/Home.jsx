5431e9f7182c19d7231009be73d2364011028488
valium69mg <xdeus.v2@gmail.com>
2024-03-21 18:42:53 -0500

    Finished the home page structure aswell as the best seller cards, all styled but no functionality on links and anchors

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -2,6 +2,54 @@ import React from "react";
 import MenuBar from "./MenuBar";
 import {Link} from 'react-router-dom';
 
+const sizes = ["24","25","26","27","28","29"];
+
+const cardInfo = [
+    {
+        id:"1",
+        name:"IUV Boots",
+        description:"Men's Cowboy Boots, Western Boots Modern.",
+        price:"53.99",
+        imgUrl:"https://m.media-amazon.com/images/I/7165U51xR7L._AC_SY695_.jpg"
+    },
+    {
+        id:"2",
+        name:"Jeossy 9806",
+        description:"Half-Banded Boots with Inner Zipper.",
+        price:"39.99",
+        imgUrl:"https://m.media-amazon.com/images/I/71NmbI3etXL._AC_SY695_.jpg"
+    },
+    {
+        id:"3",
+        name:"IUV Boots",
+        description:"Men's Cowboy Boots, Western Boots Modern.",
+        price:"53.99",
+        imgUrl:"https://m.media-amazon.com/images/I/7165U51xR7L._AC_SY695_.jpg"
+    }
+]
+
+
+function Card(props){
+    return (
+        <div className="previewCard">
+            <img src={props.imgUrl} alt={`boot${props.id}`}></img>
+            <h2> {props.name}</h2>
+            <h3> {props.description}</h3>
+            <p>{props.price}</p>
+            <div className="sizeContainer">
+                <label for="sizeLabel">Choose a size (cm):</label>
+                <select name="sizes" id="sizesId">
+                    {sizes.map((size) => {
+                        return (<option value={size}>{size}</option>);
+                    })}
+                </select>
+            </div>
+            <Link href="/" className="addToCart"> add to cart </Link>           
+        </div>
+    )
+}
+
+
 
 
 export default function Home() {
@@ -17,7 +65,19 @@ export default function Home() {
             </div>
             <img src="../imgs/cowboyboots.png" alt="cowboy boots"></img>
            </div>
-
+           
+           <div className="homeCards">
+           <h2 id="bestSellersHeading"> Our best sellers. </h2>
+                {cardInfo.map((element => {
+                    return (<Card
+                        name={element.name}
+                        description={element.description}
+                        price={element.price}
+                        imgUrl={element.imgUrl}
+                        id={element.id}
+                    />)
+                }))}
+           </div>
         </div>
     )
 }
